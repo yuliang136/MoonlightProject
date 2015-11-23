f6b119f2e1139277ed1d7f7536ce3acc3b46b87c
yuliang136 <109827298@qq.com>
2015-11-23 15:14:29 +0800

    完成基本代码
    完成基本代码

---
+++ 网络编程Client/UdpClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建socket.
+            Socket udpClient = new Socket(  AddressFamily.InterNetwork,
+                                            SocketType.Dgram,
+                                            ProtocolType.Udp);
+
+
+            // 发送数据.
+            string strIp = "192.168.2.72";
+            EndPoint serverPoint = new IPEndPoint(IPAddress.Parse(strIp), 7788);
+            string message = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            udpClient.SendTo(data, serverPoint);
+
+            Console.ReadKey();
+        }
+    }
+}
